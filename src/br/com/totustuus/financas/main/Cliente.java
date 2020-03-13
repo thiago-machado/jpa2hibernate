a1dd26c0742181ae0f88611074c59f023eb57f40
Thiago Machado <ti-thiago@outlook.com>
2020-03-13 09:38:36 -0300

    Finalização da Aula 5

--- src/br/com/totustuus/financas/main/Cliente.java
+++ src/br/com/totustuus/financas/main/Cliente.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 
 import br.com.totustuus.financas.model.Conta;
@@ -23,7 +24,14 @@ public class Cliente {
 	 * conta não pode pertencer a mais de um cliente. Portanto, o relacionamento
 	 * entre Cliente e Conta é de "um para um". Usaremos a anotação @OneToOne para
 	 * representá-lo.
+	 * 
+	 * A anotação @OneToOne por padrão não nos garante essa restrição, portanto é
+	 * necessário adicionarmos a anotação @JoinColumn(unique=true).
+	 * 
+	 * Para que @JoinColumn funcione, é necessário que as tabelas sejam criadas com 
+	 * esse comando essa anotação. Caso contrário, nunca teremos um registro 1:1
 	 */
+	@JoinColumn(unique = true)
 	@OneToOne
 	private Conta conta;
 
---
+++ src/br/com/totustuus/financas/main/TestaContaCliente.java
@@ -0,0 +1,38 @@
+package br.com.totustuus.financas.main;
+
+import javax.persistence.EntityManager;
+
+import br.com.totustuus.financas.model.Conta;
+import br.com.totustuus.financas.util.JPAUtil;
+
+public class TestaContaCliente {
+
+	public static void main(String[] args) {
+		
+		Cliente cliente = new Cliente();
+        cliente.setNome("Leonardo");
+        cliente.setEndereco("Rua Fulano, 123");
+        cliente.setProfissao("Professor");
+        
+        Cliente cliente2 = new Cliente();
+        cliente2.setNome("Douglas");
+        cliente2.setEndereco("Rua Fulano, 234");
+        cliente2.setProfissao("Professor");
+
+        Conta conta = new Conta();
+        conta.setId(2);
+
+        cliente.setConta(conta);
+        cliente2.setConta(conta);
+
+        EntityManager em = JPAUtil.getEntityManagerFactoty().createEntityManager();
+        em.getTransaction().begin();
+
+        em.persist(cliente);
+        //em.persist(cliente2);
+        // esse registro não será inserido pois não podemos ter dois clientes para uma mesma conta
+
+        em.getTransaction().commit();  
+        
+	}
+}
